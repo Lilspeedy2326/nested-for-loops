23969b66913c0906f0fa8bb4886d8db8ae087a49
Lilspeedy2326 <146358585+Lilspeedy2326@users.noreply.github.com>
2024-01-23 10:02:17 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+for i in range(2):
+    for j in range(3):
+        print("*", end = " ")
+    print()
+    
+for i in range(5):
+    for j in range(5):
+        print(j, end = " ")
+    print()
+
+for i in range(5):
+    for j in range(i):
+        print("*", end = " ")
+    print()
+    
+
